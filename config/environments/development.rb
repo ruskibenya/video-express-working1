8b0e88ab5b38f01ea5ec89c425df4398b3480851
Benjamin Aronov <benjamin@benyas.com>
2022-06-28 14:17:31 +0300

    Moved home page styles to video.scss

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,7 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.hosts << "e07b-77-73-159-12.ngrok.io"
+  config.hosts << "70e8-77-73-159-12.ngrok.io"
 
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
